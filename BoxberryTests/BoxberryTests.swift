5f8d350b129bf27a6c62f76ebef780862c235462
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-11-16 00:03:05 +0700

    Initial Commit

---
+++ BoxberryTests/BoxberryTests.swift
@@ -0,0 +1,34 @@
+//
+//  BoxberryTests.swift
+//  BoxberryTests
+//
+//  Created by Евгений Бижанов on 16/11/2018.
+//  Copyright © 2018 Евгений Бижанов. All rights reserved.
+//
+
+import XCTest
+@testable import Boxberry
+
+class BoxberryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
