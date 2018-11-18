e540d5e6366161f14e31ca689a6aa82c5a0364d3
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-11-18 17:41:29 +0300

    Добавлен контроллер для отображения точек заказа

---
+++ Boxberry/Flows/Points/PointsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PointsViewController.swift
+//  Boxberry
+//
+//  Created by Евгений Бижанов on 18/11/2018.
+//  Copyright © 2018 Евгений Бижанов. All rights reserved.
+//
+
+import UIKit
+
+class PointsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
