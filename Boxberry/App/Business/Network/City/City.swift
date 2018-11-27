561ade489c4b7e2849d133ae59501ff4e81df463
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-11-27 17:14:07 +0300

    Добавлены поля в структуру city согласно документации

--- Boxberry/App/Business/Network/City/City.swift
+++ Boxberry/App/Business/Network/City/City.swift
@@ -7,5 +7,19 @@
 //
 
 struct City: Decodable {
-    
+    let code: String
+    let name: String
+    let receptionLaP: String
+    let deliveryLaP: String
+    let reception: String
+    let pickupPoint: String?
+    let courierDelivery: String?
+    let foreignReceptionReturns: String
+    let terminal: String
+    let kladr: String
+    let region: String
+    let countryCode: String
+    let uniqName: String
+    let district: String
+    let prefix: String
 }
